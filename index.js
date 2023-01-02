7f718ad17ac154e84469bdae08df8271eab980ad
Bhavya Bansal <bhavyabansal0916@gmail.com>
2023-01-02 17:39:32 +0530

    reading indeed.com using puppeteer2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const getRoutes = require('./routes/getRoutes');
+const ngrok = require('ngrok');
 const PORT = 3000;
 app.use(express.json());
 app.use(express.static("public"));
@@ -8,4 +9,18 @@ app.use(express.urlencoded({extended:false}));
 app.use('/api/v1',getRoutes);
 app.listen(PORT,()=>{
     console.log('Server is running'); 
-})
+});
+//NODEMON
+//To keep track of changes we use nodemon
+// It Keeps tracks and restart the server at every change
+//NGROK
+// ngrok is used to share the code without pushing it on any server
+// it connects your localhost and make it public for sometime
+// (async function(){
+//     const url = await ngrok.connect({
+//         proto:'http',
+//         addr:PORT,
+//         authtoken:""
+//     });
+//     console.log(url);
+// })()//self calling function of JS
--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,5 +1,8 @@
+const { response } = require('express');
 const express = require('express');
 const routes = express.Router();
+const fs = require('fs');
+const path = require('path');
 const main = require('../scrapfn/scrape'); 
 routes.post('/indeed',async (req,res)=>{
     try{
@@ -16,5 +19,12 @@ routes.post('/indeed',async (req,res)=>{
         return res.status(500).send(e);
     }
 })
-
+routes.get('/getData',async (req,res)=>{
+    try{
+        const jobs = path.join(__dirname,'../','job.json');
+        return res.status(200).sendFile(jobs);
+    }catch(e){
+        return res.status(500).send(e);
+    }
+})
 module.exports = routes;
